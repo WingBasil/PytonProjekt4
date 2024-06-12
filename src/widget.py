6204dd040002ab2e88b29a932051246915fa6783
WingBasil <i@vyarygin.ru>
2024-06-12 22:55:55 +0700

    final version

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,8 @@
-from typing import Union
+from typing import Any
 import masks
-def mask_account_card(input_number: str) -> str:
+
+
+def mask_account_card(input_number: str) -> Any:
     """Анализирует принятую строку на наличие информации о счете или карте
     Формирует строку с Типом карты/Счетом + маска."""
     if "Счет" in input_number:
@@ -8,15 +10,16 @@ def mask_account_card(input_number: str) -> str:
         new_text = "Счет " + masks.get_mask_account(account_number)
     else:
         card_number = int(input_number[-16:])
-        new_text =  input_number[:15] + masks.get_mask_card_number(card_number)
+        new_text = input_number[:15] + masks.get_mask_card_number(card_number)
     return new_text
 
-def get_data(date: str) -> str:
+
+def get_data(date: str) -> Any:
     """Функция преобразования даты"""
     return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
 
-print(get_data('2018-07-11T02:26:18.671407'))
 
-#input_number = "MasterCard 7158300734726758"
-#input_number = "Счет 12345678901234567890"
-#print(mask_account_card(input_number))
+# print(get_data('2018-07-11T02:26:18.671407'))
+# input_number = "MasterCard 7158300734726758"
+# input_number = "Счет 12345678901234567890"
+print(mask_account_card(input_number))
