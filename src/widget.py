01c3ddc0f2b4bcc748ee96fd3d7d6bdd8836e478
WingBasil <i@vyarygin.ru>
2024-06-18 21:31:04 +0700

    add test account_card, get_date

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,5 @@
 from typing import Any
-import masks
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(input_number: str) -> Any:
@@ -7,10 +7,10 @@ def mask_account_card(input_number: str) -> Any:
     Формирует строку с Типом карты/Счетом + маска."""
     if "Счет" in input_number:
         account_number = int(input_number[-20:])
-        new_text = "Счет " + masks.get_mask_account(account_number)
+        new_text = "Счет " + get_mask_account(account_number)
     else:
         card_number = int(input_number[-16:])
-        new_text = input_number[:15] + masks.get_mask_card_number(card_number)
+        new_text = input_number[:15] + get_mask_card_number(card_number)
     return new_text
 
 
@@ -19,7 +19,7 @@ def get_data(date: str) -> Any:
     return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
 
 
-# print(get_data('2018-07-11T02:26:18.671407'))
+print(get_data('2018-07-11T02:26:18.671407'))
 input_number = "MasterCard 7158300734726758"
 # input_number = "Счет 12345678901234567890"
 print(mask_account_card(input_number))
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,5 @@
 import pytest
-from src.widget import get_mask_card_number
-from src.masks import get_mask_card_number
+from src.widget import mask_account_card, get_data
 
 
 @pytest.fixture
@@ -13,3 +12,9 @@ def input_number():
 
 def test_mask_account_card(input_number):
     assert mask_account_card(input_number) == "MasterCard 71587158 30** **** 6758"
+
+@pytest.fixture
+def date():
+    return "2018-07-11T02:26:18.671407"
+def test_get_data(date):
+    assert get_data(date) == "11.07.2018"
