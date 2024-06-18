f9f4107a67784933135bbde62bebbaafc0187e49
WingBasil <i@vyarygin.ru>
2024-06-18 22:20:34 +0700

    add test account_card, get_date

--- src/widget.py
+++ src/widget.py
@@ -10,7 +10,7 @@ def mask_account_card(input_number: str) -> Any:
         new_text = "Счет " + get_mask_account(account_number)
     else:
         card_number = int(input_number[-16:])
-        new_text = input_number[:15] + get_mask_card_number(card_number)
+        new_text = input_number[:-16] + get_mask_card_number(card_number)
     return new_text
 
 
@@ -19,7 +19,7 @@ def get_data(date: str) -> Any:
     return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
 
 
-print(get_data('2018-07-11T02:26:18.671407'))
-input_number = "MasterCard 7158300734726758"
+# print(get_data('2018-07-11T02:26:18.671407'))
+# input_number = "MasterCard 7158300734726758"
 # input_number = "Счет 12345678901234567890"
-print(mask_account_card(input_number))
+# print(mask_account_card(input_number))
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,19 +2,18 @@ import pytest
 from src.widget import mask_account_card, get_data
 
 
-@pytest.fixture
-def input_number():
-    return "MasterCard 7158300734726758"
+@pytest.mark.parametrize("string, expected_result", [
+    ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+    ("Счет 12345678901234567890", "Счет **7890"),
+    ])
+def test_mask_account_card(string, expected_result):
+    assert mask_account_card(string) == expected_result
 
-#@pytest.fixture
-#def account_number():
-#    return "Счет 12345678901234567890"
-
-def test_mask_account_card(input_number):
-    assert mask_account_card(input_number) == "MasterCard 71587158 30** **** 6758"
 
 @pytest.fixture
 def date():
     return "2018-07-11T02:26:18.671407"
+
+
 def test_get_data(date):
     assert get_data(date) == "11.07.2018"
