0f4268136574018663a834f9be7560e3671f302a
WingBasil <i@vyarygin.ru>
2024-06-26 15:55:00 +0700

    add scan no file

--- src/decorators.py
+++ src/decorators.py
@@ -12,23 +12,22 @@ def log(filename: Any) -> Callable:
             try:
                 result = func(*args, **kwargs)
                 log_message = f"{func.__name__} called with args: {args}, kwargs:{kwargs}. Result: {result}"
+            except Exception as e:
+                log_message = f"{func.__name__} error: {e}. Inputs:{args}, {kwargs}"
+            if filename:
                 with open(filename, "a") as f:
                     f.write(log_message + "\n")
+            else:
                 print(log_message)
-            except Exception as e:
-                error_message = f"{func.__name__} error: {e}. Inputs:{args}, {kwargs}"
-                with open(filename, "a") as f:
-                    f.write(error_message + "\n")
-                print(error_message)
-
         return wrapper
 
     return decorator
 
 
-@log(filename="test_log.txt")
+#@log(filename="test_log.txt")
+@log(filename="")
 def my_function(x: int, y: int) -> int:
     return x + y
 
 
-my_function(1, 2)
+my_function(1,2)
